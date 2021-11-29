c5c2c0c9b8a2e996e11db0bb3b8c38095f187a3e
Clairegetz <clairegetz@email.arizona.edu>
2021-11-29 10:07:48 -0700

    first push

---
+++ Proj4.java
@@ -0,0 +1,8 @@
+public class Proj4 {
+    public static void main(String[] args) {
+        //ToDo:record insertion
+        //ToDo:record deletion
+        //ToDO:record update
+        //ToDo:queries
+    }
+}
