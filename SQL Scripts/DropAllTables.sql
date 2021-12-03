3e5cb35148dfe8c56b4e1e1b225ae5252a7a3606
Claire M Getz <clairegetz@cs.arizona.edu>
2021-12-02 18:45:08 -0700

    added scripts to build and drop tables

---
+++ SQL Scripts/DropAllTables.sql
@@ -0,0 +1,13 @@
+--Not used as foreign key
+drop table Appointment;
+drop table License;
+drop table Permit;
+drop table Registration;
+drop table StateID;
+
+--Used as foreign key in above
+drop table Customer;
+drop table Employee;
+drop table Job;
+drop table Department;
+
