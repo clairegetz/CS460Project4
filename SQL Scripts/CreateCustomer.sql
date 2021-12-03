125db04deb8df1cb0c113fd1205d0490e77034d9
Jiazheng <jasonhjz9527@gmail.com>
2021-12-03 10:28:21 -0800

    Added random data to each tables

--- SQL Scripts/CreateCustomer.sql
+++ SQL Scripts/CreateCustomer.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Customer(
        customerID INT NOT NULL,
-       firstName VARCHAR(20) NOT NULL,
-       lastName VARCHAR(30) NOT NULL,
-       address VARCHAR(200) NOT NULL,
+       firstName VARCHAR(10) NOT NULL,
+       lastName VARCHAR(10) NOT NULL,
+       address VARCHAR(30) NOT NULL,
        dateOfBorth DATE NOT NULL,
        PRIMARY KEY(customerID)
 );
