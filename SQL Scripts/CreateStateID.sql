696c01a7605645aadbbd8af1e97607b484323488
Clairegetz <clairegetz@email.arizona.edu>
2021-12-06 09:56:42 -0700

    added data to populate

--- SQL Scripts/CreateStateID.sql
+++ SQL Scripts/CreateStateID.sql
@@ -1,7 +1,7 @@
 CREATE TABLE StateID (
        stateIDNo VARCHAR(20) NOT NULL,
        issueDate DATE,  -- need to change
-       expireDate DATE AS add_months(issueDate, 240)),
+       expireDate DATE AS (add_months(issueDate, 240)),
        customerID INT NOT NULL,
        deptID INT NOT NULL,
        PRIMARY KEY(stateIDNo),
